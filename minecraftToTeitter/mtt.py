636374497bc6dce0aafcfa222eb98304ac7994b9
su-2 <su-2@ume-ch.net>
2016-08-01 22:04:43 +0900

    add function tweet OAuth

--- minecraftToTeitter/mtt.py
+++ minecraftToTeitter/mtt.py
@@ -3,3 +3,34 @@
 minecraft twitter 連携tool
 
 """
+from requests_oauthlib import OAuth1Session
+from docutils.languages import cs
+
+
+class tweetOAuth():
+
+    def __init__(self, CK, CS, AT, AS):
+        self.CK = CK
+        self.CS = CS
+        self.AT = AT
+        self.AS = AS
+        self.header = ""
+        self.payload = ""
+        self.footer = ""
+
+    def twittDo(self):
+        self.url = "https://api.twitter.com/1.1/statuses/update.json"
+        self.params = {
+            "status": self.header + "\n" + self.payload + "\n" + self.footer}
+        self.twitter = OAuth1Session(self.CK, self.CS, self.AT, self.AS)
+        self.req = self.twitter.post(self.url, params=self.params)
+        return self.req
+
+    def SetHeader(self, str):
+        self.header = str
+
+    def SetPayload(self, str):
+        self.payload = str
+
+    def SetFooter(self, str):
+        self.footer = str
